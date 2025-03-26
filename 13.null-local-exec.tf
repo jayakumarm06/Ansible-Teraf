44cc7a63c6bdcffdfb1cec4e6a30640b7369d986
jai <jaikumarm061286@gmail.com>
2025-03-26 20:10:44 +0530

    added the files inline indentation

--- 13.null-local-exec.tf
+++ 13.null-local-exec.tf
@@ -1,8 +1,7 @@
 resource "null_resource" "webservers" {
   provisioner "local-exec" {
     command = <<-EOF
-      sleep 10
-      ansible -i invfile pvt -m ping
+      sleep 10 && ansible -i invfile pvt -m ping
     EOF
   }
   depends_on = [local_file.ansible-inventory-file]
