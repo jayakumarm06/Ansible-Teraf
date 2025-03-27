d4f6c298b64db63382da422bed1ac194fb5c7c64
jai <jaikumarm061286@gmail.com>
2025-03-27 18:25:54 +0530

    Modified the bucket name

--- 1.provider.tf
+++ 1.provider.tf
@@ -11,7 +11,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "ansible-bucke-terraf-01"
+    bucket         = "ansible-bucke-terraf-02"
     key            = "Ansible.tfstate"
     region         = "us-east-1"
     # dynamodb_table = "-terraform-locks"
